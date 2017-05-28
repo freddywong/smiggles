778a69b37dcd2615c9314a2b6ca02096dbb468cb
Frederick Wong <frederick.kf.wong@gmail.com>
2017-05-28 14:51:31 +0900

    Added facebook api id and secret env references

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, 'YOUR-APP-ID-HERE', 'YOUR-APP-SECRET-HERE'
+  provider :facebook, ENV['FACEBOOK_API_ID'], ENV['FACEBOOK_API_SECRET']
 end
