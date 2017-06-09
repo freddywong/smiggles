a56a9d21bb6be2730dbd9a68ad95d203982cfbff
Frederick Wong <frederick.kf.wong@gmail.com>
2017-06-09 19:19:20 +0900

    Styled the login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: { sessions: 'users/sessions', :omniauth_callbacks => "users/omniauth_callbacks" }
+  
   mount ActionCable.server => '/cable'
   root 'smiggles#edit'
   resources :smiggles, only: [:edit, :update]
