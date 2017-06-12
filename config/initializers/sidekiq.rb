f9dcfe7857f28973b4fffa72be83f19d7446778b
Frederick Wong <frederick.kf.wong@gmail.com>
2017-06-12 17:50:01 +0900

    Added sidekiq initializer

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,10 @@
+if Rails.env.production?
+  Sidekiq.configure_client do |config|
+    config.redis = { url: ENV['REDISTOGO_URL'], size: 1}
+  end
+
+  Sidekiq.configure_server do |config|
+    config.redis = { size: 2 }
+  end
+end
+
