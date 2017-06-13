1007f9b3bf6f306c795048898d6555bc515996e9
Frederick Wong <frederick.kf.wong@gmail.com>
2017-06-13 16:41:34 +0900

    Removed puma workers as a work around to an 'firstpos' error I got starting a rails server on development

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,6 @@
 if Rails.env.production?
   Sidekiq.configure_client do |config|
-    config.redis = { url: ENV['REDISTOGO_URL'], size: 4}
+    config.redis = { url: ENV['REDISTOGO_URL'], size: 2}
   end
 
   Sidekiq.configure_server do |config|
