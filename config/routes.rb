fae14eb2f0b1619acf687c1b860f225be30457bf
Frederick Wong <frederick.kf.wong@gmail.com>
2017-07-21 09:44:30 +0800

    Removed unnecessary registration and password routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,20 @@
 Rails.application.routes.draw do
   require 'sidekiq/web'
   mount Sidekiq::Web => '/sidekiq'
-  devise_for :users, controllers: { sessions: 'users/sessions', :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_for :users, 
+    controllers: { sessions: 'users/sessions', :omniauth_callbacks => "users/omniauth_callbacks" },
+    skip: [:registrations, :passwords]
+
+  devise_scope :user do
+    resource :registration,
+      only: [:new, :create],
+      path: 'users',
+      path_names: { new: 'sign_up' },
+      controller: 'devise/registrations',
+      as: :user_registration do
+        get :cancel
+      end
+  end
   
   mount ActionCable.server => '/cable'
   root 'smiggles#edit'
